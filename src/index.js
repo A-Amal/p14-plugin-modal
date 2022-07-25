c3eef49acd54155849142f6bb7ce206dc8b3928d
Abdelkafi Amal <zaghdanamal@yahoo.fr>
2022-07-25 16:07:30 +0200

    modif modal.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Modal from "./lib/components/Modal";
-import {ModalIcon} from "./lib/components/ModalIcon";
 import {ModalTitle} from "./lib/components/ModalTitle";
 import {ModalContent} from "./lib/components/ModalContent";
 import {ModalActions} from "./lib/components/ModalActions";
-import { ReactComponent as GreatStarSVG } from './asset/logo.svg'
 
 
 const close = true
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <Modal name="my-super-bright-alert" show={true} setShow={ false }>
-          <ModalTitle>This is my super bright alert</ModalTitle>
-          <ModalContent>Don't forget to follow me on my github account</ModalContent>
+      <Modal name="my-super-bright-alert">
+          <ModalTitle> Modal Title</ModalTitle>
+          <ModalContent>*****Modal Message*****</ModalContent>
           <ModalActions>
-              <button type="button" onClick={() => close}>It was famous</button>
+              <button type="button" onClick={() =>console.log(close)}>Close Modal</button>
           </ModalActions>
       </Modal>
   </React.StrictMode>
--- src/lib/components/Modal.js
+++ src/lib/components/Modal.js
@@ -1,4 +1,4 @@
-import {createContext, useCallback, useEffect, useRef} from "react";
+import {createContext, useCallback, useEffect, useRef, useState} from "react";
 import propTypes from 'prop-types'
 import "./Modal.css"
 
@@ -13,16 +13,14 @@ export const ModalContext = createContext()
  * @param {Object} params
  * @param {String} params.name
  * @param {Boolean} params.showClose
- * @param {Boolean} params.show
- * @param {Function} params.setShow
  */
-export default function Modal({ name, showClose = true, show, setShow, children }) {
+export default function Modal({ name, showClose = true, children }) {
 
     /**
      * References
      */
     const ref = useRef(null)
-
+    const [show, setShow] = useState(true)
     /**
      * Manage keyboard interaction
      * @param  e
@@ -32,9 +30,9 @@ export default function Modal({ name, showClose = true, show, setShow, children 
     }, [setShow])
     // Focus first element focusable
     useEffect(() => {
-        if (show) {
-            ref.current.querySelectorAll('button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])')[0].focus()
-            document.addEventListener('keydown', handelKeydown)
+          if (show) {
+              ref.current.querySelectorAll('button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])')[0].focus()
+              document.addEventListener('keydown', handelKeydown.bind(this))
         } else {
             document.removeEventListener('keydown', handelKeydown)
         }
@@ -47,7 +45,7 @@ export default function Modal({ name, showClose = true, show, setShow, children 
      * Render
      */
     return show && (
-        <ModalContext.Provider value={{show, setShow}}>
+        <ModalContext.Provider value={{show, setShow, showClose, children}}>
             <div className="modal" aria-labelledby={`${name}-title`} role="dialog" aria-modal="true" ref={ref}>
                 <div className="modal-container">
                     <div className="modal-overlay" aria-hidden="true" onClick={() => setShow(!show)}/>
@@ -57,10 +55,7 @@ export default function Modal({ name, showClose = true, show, setShow, children 
                             {children}
                         </div>
                         {showClose &&
-                            <button
-                                className="modal-close"
-                                onClick={() => setShow(!show)}
-                            >
+                            <button className="modal-close" onClick={() => setShow(!show)}>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M193.94 256L296.5 153.44l21.15-21.15c3.12-3.12 3.12-8.19 0-11.31l-22.63-22.63c-3.12-3.12-8.19-3.12-11.31 0L160 222.06 36.29 98.34c-3.12-3.12-8.19-3.12-11.31 0L2.34 120.97c-3.12 3.12-3.12 8.19 0 11.31L126.06 256 2.34 379.71c-3.12 3.12-3.12 8.19 0 11.31l22.63 22.63c3.12 3.12 8.19 3.12 11.31 0L160 289.94 262.56 392.5l21.15 21.15c3.12 3.12 8.19 3.12 11.31 0l22.63-22.63c3.12-3.12 3.12-8.19 0-11.31L193.94 256z"/></svg>
                             </button>
                         }
@@ -74,8 +69,7 @@ export default function Modal({ name, showClose = true, show, setShow, children 
 Modal.propTypes = {
     name: propTypes.string.isRequired,
     showClose: propTypes.bool,
-    show: propTypes.bool.isRequired,
-    setShow: propTypes.func.isRequired,
+
 }
 
 
